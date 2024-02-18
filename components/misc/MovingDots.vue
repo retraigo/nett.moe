1525b06bd26a7d3e9dad2edb1fd45ef4ca42bf47
NeTT <nett@nett.moe>
2024-02-18 13:06:09 +0530

    pls no log

--- components/misc/MovingDots.vue
+++ components/misc/MovingDots.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
+import { Points } from '~/utils/points';
+
 const colors = ["#ffe6f9", "#ffb3ed", "#ff80e1", "#ff4dd5", "#ff1ac9"];
-const arr = new Array(15).fill(null);
+const arr = new Array(23).fill(null);
 const _items: [number, number, string][] = [];
 for (const _n in arr) {
     _items.push([
@@ -24,10 +26,12 @@ const position = computed(() => {
 
 function moveItems() {
     const __items: [number, number, string][] = [];
-    for (const _n in items) {
+    const newPoint = Points[Math.floor(Math.random() * Points.length)]
+    for (let n = 0; n < items.value.length; n += 1) {
+        const point = newPoint.points[n]
         __items.push([
-            Math.floor(Math.random() * 100),
-            Math.floor(Math.random() * 100),
+            point === "r" ? Math.floor(Math.random() * 100) : !point ? Math.floor(Math.random() * 100) : point[0],
+            point === "r" ? Math.floor(Math.random() * 100) : !point ? Math.floor(Math.random() * 100) : point[1],
             colors[Math.floor(Math.random() * colors.length)]
         ]);
     }
@@ -53,8 +57,7 @@ onMounted(() => {
             <marker :id="`endLine${i}`" class="relative" viewBox="0 0 22 22" refX="10" refY="10"
                 markerUnits="userSpaceOnUse" markerWidth="5" markerHeight="5">
                 <circle class="transition-all duration-[5000ms] ease-in transform absolute -z-30 rounded-full" cx="10"
-                    :stroke="poss[2]"
-                    :fill="poss[2]" cy="10" r="10px" />
+                    :stroke="poss[2]" :fill="poss[2]" cy="10" r="10px" />
             </marker>
             <marker :id="`startLine${i}`" class="relative" viewBox="0 0 22 22" refX="10" refY="10"
                 markerUnits="userSpaceOnUse" markerWidth="5" markerHeight="5">
@@ -63,7 +66,7 @@ onMounted(() => {
             </marker>
             <ClientOnly fallback-tag="span" fallback="Xue">
                 <path :d="`${`M ${(position[0] * (poss[0])) / 100} ${(position[1] * (poss[1])) / 100}`} L ${(position[0] * (items[i + 1]?.[0] || poss[0])) / 100} ${position[1] * (items[i + 1]?.[1]
-                    || poss[1]) / 100}`" stroke-width="0.5px"
+                    || poss[1]) / 100}`" stroke-width="0.5px" v-if="i !== items.length - 1"
                     :class="`opacity-50 transition-all duration-[5000ms] ease-in transform item${i || 99}`"
                     :stroke="poss[2]" :marker-end="`url(#endLine${i})`"
                     :marker-start="!i ? `url(#startLine${i})` : undefined" />
