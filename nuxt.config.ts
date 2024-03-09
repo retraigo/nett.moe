578186f74604831cfa21334d2bc706071d658fc5
NeTT <nett@nett.moe>
2024-03-09 18:08:20 +0530

    add title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: [
-        "@nuxtjs/tailwindcss"
-    ]
-})
+  app: {
+    head: {
+      title: "NeTT - Incomplete Salty Weeb Dev",
+      meta: [
+        {
+          name: "description",
+          content:
+            "He who paints thy world in eternal nothingness... and writes weird javascript programs and websites. Also makes up cringe monologues and narrations. I write ML stuff for TypeScript (Deno).",
+        },
+        {
+          name: "og:thumbnail",
+          content: "glitchnett_promax.png",
+        },
+      ],
+    },
+  },
+  modules: ["@nuxtjs/tailwindcss"],
+});
